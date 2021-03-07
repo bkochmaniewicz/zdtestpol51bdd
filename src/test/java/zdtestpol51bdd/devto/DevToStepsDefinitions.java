78ea26d49383e78ee5fc430c11f71764f4bbfb1d
bkochmaniewicz <bartekkochmaniewicz@gmail.com>
2021-03-07 15:03:36 +0100

    use snippet testing in searchBar testing

--- src/test/java/zdtestpol51bdd/devto/DevToStepsDefinitions.java
+++ src/test/java/zdtestpol51bdd/devto/DevToStepsDefinitions.java
@@ -84,16 +84,26 @@ public class DevToStepsDefinitions {
 
     @Then("Top {int} blogs found should have correct phrase in title")
     public void top_blogs_found_should_have_correct_phrase_in_title(Integer int1) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3.crayons-story__title"))); //h3
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3.crayons-story__title")));
         wait.until(ExpectedConditions.attributeContains(By.id("substories"), "class", "search-results-loaded"));
-        List<WebElement> allPosts = driver.findElements(By.cssSelector(".crayons-story__title > a")); // a
+        List<WebElement> allPosts = driver.findElements(By.className("crayons-story__body"));
         if (allPosts.size() >= int1) {
             for (int i = 0; i < int1; i++) {
                 WebElement singlePost = allPosts.get(i);
-                String singlePostTitle = singlePost.getText().toLowerCase(Locale.ROOT); // a wyciagaj text
-                boolean isTestingInTitle = singlePostTitle.contains(searchingPhrase);
-                Assert.assertTrue(isTestingInTitle);
+                WebElement singlePostTitle = singlePost.findElement(By.cssSelector(".crayons-story__title > a"));
+                String singlePostTitleText = singlePost.getText().toLowerCase(Locale.ROOT);
+                boolean isPhraseInTitle = singlePostTitleText.contains(searchingPhrase);
+                if (isPhraseInTitle) {
+                    Assert.assertTrue(isPhraseInTitle);
+                } else {
+                    WebElement snippet = singlePost.findElement(By.xpath("//div[contains(@class, 'crayons-story__snippet')]"));
+                    String snippetText = snippet.getText().toLowerCase(Locale.ROOT);
+                    boolean isPhraseInSnippet = snippetText.contains(searchingPhrase);
+                    Assert.assertTrue(isPhraseInSnippet);
+                }
             }
         }
     }
 }
+
+
